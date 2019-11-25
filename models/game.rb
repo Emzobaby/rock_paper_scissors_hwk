b35b74e24f4b1a9784ab334bfa0b9dbe7037d727
Emma <emmagray183@gmail.com>
2019-11-25 19:58:31 +0000

    player1/player2 added

--- models/game.rb
+++ models/game.rb
@@ -1,13 +1,19 @@
 class Game
 
-def self.return_winner(option1, option2)
-  if (option1 == "rock" && option2 == "scissors") || (option1 == "scissors" && option2 == "rock")
-    return "Rock wins"
-  elsif (option1 == "paper" && option2 == "rock") || (option1 == "rock" && option2 == "paper")
-    return "Paper wins"
-  elsif (option1 == "scissors" && option2 == "paper") || (option1 == "paper" && option2 == "scissors")
-    return "Scissors wins"
+  def self.return_winner(option1, option2)
+    if (option1 == "rock" && option2 == "scissors")
+      return "Player 1 wins by playing ROCK!"
+    elsif (option1 == "scissors" && option2 == "rock")
+      return "Player 2 wins by playing ROCK!"
+    elsif (option1 == "paper" && option2 == "rock")
+      return "Player 1 wins by playing PAPER!"
+    elsif (option1 == "rock" && option2 == "paper")
+      return "Player 2 wins by playing PAPER!"
+    elsif (option1 == "scissors" && option2 == "paper")
+      return "Player 1 wins by playing SCISSORS!"
+    elsif (option1 == "paper" && option2 == "scissors")
+      return "Player 2 wins by playing SCISSORS!"
+    end
   end
-end
 
 end
